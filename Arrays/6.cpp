ee82b9a255a982d85bf6b376e63b5b573e9ec256
Reetam Dey <131424020+ReetzDev@users.noreply.github.com>
2023-08-18 21:32:18 +0530

    Update 6.cpp

--- Arrays/6.cpp
+++ Arrays/6.cpp
@@ -3,18 +3,50 @@ class Solution {
 public:
     int index;
     int search(vector<int>& nums, int target) {
+
+        //time complexity O(n)
         for(int i=0;i<size(nums);i++)
         {
             if(nums[i]==target)
             {
                 return i;
             }
-            // else
-            // {    
-            //     index=-1;
-            // }
         }
         
-    return -1;
+
+        
+    //time complexity O(logn)
+    int n = nums.size();
+        int left = 0;
+        int right = n-1;
+        int mid;
+
+        //start
+        //Iterative binary search
+        while(left<=right)
+        {
+            mid = (left+right)/2;
+            //cout<<nums[mid]<<"\n";
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[mid]>=nums[left])
+            {
+                if(target<=nums[mid] && target>=nums[left])
+                    right = mid-1;
+                else
+                    left = mid+1;
+            }
+            else
+            {
+                if(target>=nums[mid] && target<=nums[right])
+                    left = mid+1;
+                else
+                    right = mid-1;
+            }
+        }
+
+        //end 
+
+        return -1;
     }
 };
